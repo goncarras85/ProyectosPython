e945ba79edaeb470a78ef80f9fb12a8bc31ed5f7
Gonzalo <goncarrass@gmail.com>
2025-12-18 03:05:10 +0100

    Primeros proyectos de aprendizaje realizados

---
+++ calcularHipotenusa.py
@@ -0,0 +1,6 @@
+
+import math
+cateto1=int(input("Dime le primer cateto"))
+cateto2=int(input("Dime el segundo cateto"))
+hipotenusa=math.sqrt(cateto1 ** 2 + cateto2 ** 2)
+print("la hipotenusa es", hipotenusa)
